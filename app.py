8dc2daa663091f1485ce08661ae648e8b111d32d
jise0oo3027-eng <jise0oo3027@gmail.com>
2026-09-01 00:00:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -261,5 +261,8 @@ def predict():
     return jsonify({"status": "success", "predictions": result})
 
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
